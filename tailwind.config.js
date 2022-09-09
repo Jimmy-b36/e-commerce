498aea56bfff5539c8458c5428813f632d65a314
James Ball <jamie.j.ball@gmail.com>
2022-09-09 17:15:36 -0400

    add DaisyUi

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,19 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    screens: {
+      xs: { max: '428px' },
+      sm: { min: '428px', max: '640px' },
+      md: { min: '640px', max: '768px' },
+      lg: { min: '768px', max: '1024px' },
+      xl: { min: '1024px' },
+    },
     extend: {
       fontFamily: { urbanist: ['Urbanist', 'sans-serif'] },
     },
   },
-  plugins: [],
+  daisyui: {
+    themes: ['autumn'],
+  },
+  plugins: [require('daisyui')],
 };
