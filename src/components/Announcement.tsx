0174aecec599ec5ab065150d1bfbcca7bb44eb81
James Ball <jamie.j.ball@gmail.com>
2022-09-09 17:15:07 -0400

    Create Announcement

---
+++ src/components/Announcement.tsx
@@ -0,0 +1,11 @@
+const Announcement = () => {
+  return (
+    <div className="alert alert-warning h-12 mb-1 rounded-t-none shadow-lg flex justify-center">
+      <div className="">
+        <span>Warning: Invalid email address!</span>
+      </div>
+    </div>
+  );
+};
+
+export default Announcement;
