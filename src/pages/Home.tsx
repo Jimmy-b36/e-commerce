0d3f503353bdd21aaa0c83d672d3b71d7fe47c23
James Ball <jamie.j.ball@gmail.com>
2022-09-12 14:53:09 -0400

    add catagories to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,14 @@
 import Announcement from '../components/Announcement';
 import NavBar from '../components/NavBar';
 import Slider from '../components/Slider';
+import Catagories from '../components/Catagories';
 const Home = () => {
   return (
     <>
       <Announcement />
       <NavBar />
       <Slider />
+      <Catagories />
     </>
   );
 };
