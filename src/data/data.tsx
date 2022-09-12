2068cb1f557c223cac5aac83cc31be75887b7448
James Ball <jamie.j.ball@gmail.com>
2022-09-11 22:58:08 -0400

    add data for carousel | needs rename

---
+++ src/data/data.tsx
@@ -0,0 +1,29 @@
+interface IData {
+  img: string;
+  title: string;
+  description: string;
+  season: string;
+}
+
+const data = [
+  {
+    img: 'https://cf.ltkcdn.net/antiques/images/std-lg/272739-1200x800-antique-tractors.webp',
+    title: 'Tractor',
+    description: 'The best ever tractor you ever did see',
+    season: 'Spring',
+  },
+  {
+    img: 'https://www.farmmanagement.pro/wp-content/uploads/2017/11/winter-tractor-620x330.jpg',
+    title: 'Tractor',
+    description: 'The Greatest tractor you ever did see',
+    season: 'Winter',
+  },
+  {
+    img: 'https://img.artpal.com/980411/18-19-3-21-13-8-7m.jpg',
+    title: 'Tractor',
+    description: 'The most awesome tractor you ever did see',
+    season: 'Autumn',
+  },
+];
+
+export default data;
