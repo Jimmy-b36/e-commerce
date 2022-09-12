96b82f9936c6ddbbad5c7f18782a55b04d22d2f8
James Ball <jamie.j.ball@gmail.com>
2022-09-12 12:22:03 -0400

    add catagory data

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -1,29 +1,55 @@
-interface IData {
+export interface ICarouselData {
+  id: number;
   img: string;
   title: string;
   description: string;
-  season: string;
+  season?: string;
+  alt: string;
 }
 
-const data = [
+export interface ICatagories {
+  id: number;
+  name: string;
+}
+
+const carousel: ICarouselData[] = [
   {
-    img: 'https://cf.ltkcdn.net/antiques/images/std-lg/272739-1200x800-antique-tractors.webp',
-    title: 'Tractor',
-    description: 'The best ever tractor you ever did see',
-    season: 'Spring',
+    id: 1,
+    img: 'https://img.freepik.com/free-vector/sticker-icons-hand-drawn-doodle_179234-169.jpg?w=1380&t=st=1662996885~exp=1662997485~hmac=a2d5aef28a93f89b76517055183637016df5ef33a09362783937a00cc0c64524',
+    title: 'Sale',
+    description: "Don't miss out on this summers sticker sale!",
+    season: 'Summer',
+    alt: 'cartoon stickers',
   },
   {
-    img: 'https://www.farmmanagement.pro/wp-content/uploads/2017/11/winter-tractor-620x330.jpg',
-    title: 'Tractor',
-    description: 'The Greatest tractor you ever did see',
-    season: 'Winter',
+    id: 2,
+    img: 'https://img.freepik.com/free-vector/set-white-paper-stickers_107791-6052.jpg?t=st=1662997683~exp=1662998283~hmac=0800ac5312ff3891b44032935f212cfb720d06893df2c005a4badc2bb5e73e97',
+    title: 'Mockups',
+    description: "Send us your designs and we'll make you an awesome mockup!",
+    alt: 'Sticker mockups',
   },
   {
+    id: 3,
     img: 'https://img.artpal.com/980411/18-19-3-21-13-8-7m.jpg',
     title: 'Tractor',
     description: 'The most awesome tractor you ever did see',
     season: 'Autumn',
+    alt: 'Autumn Tractor',
   },
 ];
 
-export default data;
+const catagories: ICatagories[] = [
+  {
+    id: 1,
+    name: 'Sticker packs',
+  },
+  {
+    id: 2,
+    name: 'Mockups',
+  },
+  {
+    id: 3,
+    name: 'Individual Stickers',
+  },
+];
+export { carousel, catagories };
