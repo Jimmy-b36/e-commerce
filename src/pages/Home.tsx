0754f9b7163bea1fc48c9c07ad328fe990c22c5b
James Ball <jamie.j.ball@gmail.com>
2022-09-11 22:58:17 -0400

    import carousel

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import Announcement from '../components/Announcement';
 import NavBar from '../components/NavBar';
+import Slider from '../components/Slider';
 const Home = () => {
   return (
     <>
       <Announcement />
       <NavBar />
+      <Slider />
     </>
   );
 };
