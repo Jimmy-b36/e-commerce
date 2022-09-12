103ffd33ad4c68a5bc2ceababc796d2c6d06ddb4
James Ball <jamie.j.ball@gmail.com>
2022-09-12 12:18:01 -0400

    change announcement

--- src/components/Announcement.tsx
+++ src/components/Announcement.tsx
@@ -1,8 +1,8 @@
 const Announcement = () => {
   return (
-    <div className="alert alert-warning h-7  rounded-none shadow-lg flex justify-center">
+    <div className="alert bg-yellow-400 h-7  rounded-none shadow-lg flex justify-center">
       <div className="">
-        <span>£1 Fish, £1 Fish</span>
+        <span>£1 Stickers, £1 Stickers</span>
       </div>
     </div>
   );
