2e04fa803ef0daf1ff6ae9e9773741b79261f585
James Ball <jamie.j.ball@gmail.com>
2022-09-12 12:18:49 -0400

    add key presses & edit styling

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,34 +1,55 @@
 import { useEffect, useState } from 'react';
-import data from '../data/data';
+import carousel from '../data/data';
+import { ICarouselData } from '../data/data';
 
 const Slider = () => {
   const [currentIndex, setCurrentIndex] = useState<number>(0);
+
   const handleClick = (direction: 'next' | 'prev') => {
-    if (direction === 'next') setCurrentIndex((currentIndex + 1) % data.length);
-    if (direction === 'prev') setCurrentIndex((currentIndex + 2) % data.length);
+    if (direction === 'next')
+      setCurrentIndex((currentIndex + 1) % carousel.length);
+    if (direction === 'prev')
+      setCurrentIndex((currentIndex + 2) % carousel.length);
   };
+
+  const handleKeyPress = (e: KeyboardEvent) => {
+    if (e.key === 'ArrowRight') handleClick('next');
+    if (e.key === 'ArrowLeft') handleClick('prev');
+  };
+
+  //useEffect to change the current display on screen
   useEffect(() => {}, [currentIndex]);
+
+  //useEffect to add keyboard left/right functionality
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyPress);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyPress);
+    };
+  }, [handleKeyPress]);
+
   return (
-    <>
+    <div className="bg-slate-50">
       <div className="flex absolute justify-between w-full items-center h-full">
         <button
-          className="btn z-30 ml-4 bg-slate-600"
+          className="btn z-10 ml-10 bg-slate-600 hover:bg-slate-800 text-white"
           onClick={() => handleClick('prev')}
         >
-          prev
+          <i className="fa-solid fa-arrow-left text-2xl"></i>
         </button>
         <button
-          className="btn z-30 mr-4 bg-slate-600"
+          className="btn z-10 mr-10 bg-slate-600 hover:bg-slate-800 text-white"
           onClick={() => handleClick('next')}
         >
-          next
+          <i className="fa-solid fa-arrow-right text-2xl"></i>
         </button>
       </div>
       <div className="flex flex-nowrap overflow-hidden">
-        {data.map((item, index) => (
+        {carousel.map((item: ICarouselData, index: number) => (
           <div
             className={
-              'h-screen w-screen flex items-center flex-col duration-500 ease-in-out shrink-0 justify-center transition ' +
+              'h-screen w-screen flex items-center flex-row duration-500 ease-in-out shrink-0 justify-center transition ' +
               '-translate-x-' +
               '[' +
               currentIndex * 100 +
@@ -37,16 +58,26 @@ const Slider = () => {
             }
             key={index}
           >
-            <p className="text-3xl underline mb-5">
-              {item.season} {item.title}
-            </p>
-            <img src={item.img} alt="" className="h-1/2 w-1/2 rounded-full" />
-            <div>{item.description}</div>
-            <button className="btn z-40">Shop now</button>
+            <img
+              src={item.img}
+              alt={item.alt}
+              className="h-2/3 w-auto rounded-3xl mb-2 mask overflow-hidden shrink-0 mr-4 object-cover"
+            />
+            <div className="flex flex-col justify-center -mt-[100px] h-full items-center">
+              <p className="text-[90px]">
+                {item.season} {item.title}
+              </p>
+              <div className="my-10 text-[20px] whitespace-normal w-2/3">
+                {item.description}
+              </div>
+              <button className="btn z-20 w-1/2 text-white bg-slate-600 hover:bg-slate-800">
+                Shop now
+              </button>
+            </div>
           </div>
         ))}
       </div>
-    </>
+    </div>
   );
 };
 
