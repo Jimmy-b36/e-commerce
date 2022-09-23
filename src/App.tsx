aa7719654beedc3a8df2e3b441e82d009f31f1ff
James Ball <jamie.j.ball@gmail.com>
2022-09-23 14:59:21 -0700

    add Product page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import Home from './pages/Home';
+import ProductList from './pages/ProductList';
+import Product from './pages/Product';
 
 function App() {
-  return <Home />;
+  return <Product />;
 }
 
 export default App;
