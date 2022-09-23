7b8ea20cfdbe5b72c919a349b1bcb4df4fe046c1
jimmy-btv <jimmy.btv22@gmail.com>
2022-09-23 13:08:01 -0700

    add import for products

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import NavBar from '../components/NavBar';
 import Slider from '../components/Slider';
 import Catagories from '../components/Catagories';
 import ContactForm from '../components/ContactForm';
-import Products from '../components/Products';
+import Products from '../components/PopularProducts';
 import Newsletter from '../components/Newsletter';
 import Footer from '../components/Footer';
 
