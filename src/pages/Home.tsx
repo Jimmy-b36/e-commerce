409f7f3ea62f44577c8defa917b97f75dffec967
James Ball <114246938+jimmy-btv@users.noreply.github.com>
2022-09-23 09:34:39 -0700

    create footer

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,6 +5,7 @@ import Catagories from '../components/Catagories';
 import ContactForm from '../components/ContactForm';
 import Products from '../components/Products';
 import Newsletter from '../components/Newsletter';
+import Footer from '../components/Footer';
 
 const Home = () => {
   return (
@@ -15,6 +16,7 @@ const Home = () => {
       <Catagories />
       <Products />
       <Newsletter />
+      <Footer />
     </>
   );
 };
