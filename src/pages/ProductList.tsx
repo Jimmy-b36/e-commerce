781c40ba9eeb3b55e5a81f641aca01b6e522e62e
James Ball <jamie.j.ball@gmail.com>
2022-09-23 14:59:34 -0700

    create All products page

---
+++ src/pages/ProductList.tsx
@@ -0,0 +1,17 @@
+import NavBar from '../components/NavBar';
+import Announcement from '../components/Announcement';
+import Footer from '../components/Footer';
+import Products from '../components/ProductPages/Products';
+
+const ProductList = () => {
+  return (
+    <>
+      <Announcement />
+      <NavBar />
+      <Products />
+      <Footer />
+    </>
+  );
+};
+
+export default ProductList;
