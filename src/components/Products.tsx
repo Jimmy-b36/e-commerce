32d0d3166832665f1dde7e7364dc4f7f24c88636
jimmy-btv <jimmy.btv22@gmail.com>
2022-09-23 12:57:45 -0700

    added general product page

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,16 +1,40 @@
-import { popularProducts } from '../data/data';
-import { IPopularProducts } from '../data/data';
 import Product from './Product';
+import { products } from '../data/data';
+import { IProducts } from '../data/data';
+import { useState } from 'react';
 const Products = () => {
+  const [productCategory, setProductCategory] = useState<string>('');
+
+  const onChangeHandler = (e: React.ChangeEvent) => {
+    setProductCategory((e.target as HTMLInputElement).value);
+  };
   return (
     <div>
-      <p className="flex justify-center text-[75px] p-5 mt-4">
-        Popular productsi
-      </p>
+      <p className="flex justify-center text-heading pt-5">{productCategory}</p>
+      <div className="flex flex-col">
+        <label htmlFor="categories" className="ml-10 p-2">
+          Pick a category:
+        </label>
+        <select
+          className="select w-full max-w-xs ml-10"
+          name="categories"
+          value={productCategory}
+          onChange={onChangeHandler}
+        >
+          <option disabled selected>
+            Category
+          </option>
+          <option>Single stickers</option>
+          <option>Sticker packs</option>
+        </select>
+      </div>
+
       <div className="flex justify-between flex-wrap p-5">
-        {popularProducts.map((item: IPopularProducts, index: number) => (
-          <Product {...item} key={index} />
-        ))}
+        {products.map((item: IProducts, index: number) =>
+          item.category === productCategory ? (
+            <Product {...item} key={index} />
+          ) : null
+        )}
       </div>
     </div>
   );
--- src/pages/ProductList.tsx
+++ src/pages/ProductList.tsx
@@ -1,11 +1,14 @@
 import NavBar from '../components/NavBar';
 import Announcement from '../components/Announcement';
 import Footer from '../components/Footer';
+import Products from '../components/Products';
+
 const ProductList = () => {
   return (
     <>
       <Announcement />
       <NavBar />
+      <Products />
       <Footer />
     </>
   );
