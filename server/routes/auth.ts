f76fc4d7c103f151fb8fe32c28488df5a40c0565
James Ball <jamie.j.ball@gmail.com>
2022-09-27 14:46:06 -0700

    add register and login endpoints

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -1,24 +1,60 @@
+require('dotenv').config();
 const router = require('express').Router();
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
 import { IRouter, Request, Response } from 'express';
 
+interface IUser {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  _doc: {
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+  };
+}
+
 // REGISTER routes
 const authRouter = (): IRouter => {
-  router.put('/register', (req: Request, res: Response) => {
-    const newUser = new User({
+  router.get('/', (req: Request, res: Response) => {
+    res.send('auth router');
+  });
+
+  router.put('/register', async (req: Request, res: Response) => {
+    const newUser = await new User({
       firstName: req.body.firstName,
       lastName: req.body.lastName,
       email: req.body.email,
-      password: bcrypt.hash(
-        req.body.password,
-        10,
-        (err: Error, hash: string) => {
-          if (err) return res.status(500).send(err);
-          return hash.toString();
-        }
-      ),
+      password: bcrypt.hashSync(req.body.password, Number(process.env.SALT)),
     });
+    try {
+      const user: IUser = await newUser.save();
+      res.status(200).send(user);
+    } catch (err) {
+      return res.status(500).send(err);
+    }
+  });
+
+  // LOGIN
+
+  router.post('/login', async (req: Request, res: Response) => {
+    try {
+      const user: IUser = await User.findOne({ email: req.body.email });
+      if (!user) return res.status(401).send('User not found');
+
+      if (bcrypt.compareSync(req.body.password, user.password)) {
+        const { password, ...others } = user._doc;
+        return res.status(200).send(others);
+      }
+      return res.status(500).send('password incorrect');
+    } catch (err) {
+      return res.status(500).send(err);
+    }
   });
   return router;
 };
+
+module.exports = authRouter;
