ef14a593482557704b0074618da548db600475f5
James Ball <jamie.j.ball@gmail.com>
2022-09-27 08:48:29 -0700

    change spacing

--- src/components/ProductPages/PopularProducts.tsx
+++ src/components/ProductPages/PopularProducts.tsx
@@ -18,7 +18,7 @@ const PopularProducts = () => {
       <p className="flex justify-center xs:text-3xl sm:text-3xl text-heading pt-5">
         Popular Products
       </p>
-      <div className="flex justify-between flex-wrap p-5">
+      <div className="flex justify-around flex-wrap p-5">
         {products.map(
           (item: IProducts, index: number) =>
             item.popular && (
