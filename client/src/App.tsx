f6066b04f43aefe1157bd64b57b719a895ba5845
jimmy-btv <jimmy.btv22@gmail.com>
2022-10-03 08:41:48 -0700

    add routes from react router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,38 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import Home from './pages/Home';
 import ProductList from './pages/ProductList';
 import Product from './pages/Product';
 import Registration from './pages/Registration';
 import Checkout from './pages/Checkout';
+import Pay from './components/Pay';
 
 function App() {
-  return <Checkout />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products/:category" element={<ProductList />} />
+        <Route path="/product/:id" element={<Product />} />
+        <Route
+          path="pay"
+          element={
+            <Pay
+              children={null}
+              cart={{
+                products: [{ productId: '12', quantity: 5 }],
+                subTotal: 1000,
+              }}
+            />
+          }
+        />
+        <Route path="/cart" />
+        {/* <Cart /> */}
+        <Route path="/success" />
+        {/* <Success /> */}
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
