7a3d865a9a0000bd7a73383ecdfcbd7a7e4005b7
jimmy-btv <jimmy.btv22@gmail.com>
2022-10-05 11:57:58 -0700

    create form for custom stickers

---
+++ client/src/pages/Custom.tsx
@@ -0,0 +1,16 @@
+import NavBar from '../components/NavBar';
+import Newsletter from '../components/Newsletter';
+import Footer from '../components/Footer';
+import ContactForm from '../components/ContactForm';
+const Custom = () => {
+  return (
+    <>
+      <NavBar />
+      <ContactForm />
+      <Newsletter />
+      <Footer />
+    </>
+  );
+};
+
+export default Custom;
