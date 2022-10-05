83f0468b1f0fedc563b9cef76a884556b2d841d4
jimmy-btv <jimmy.btv22@gmail.com>
2022-10-05 11:56:58 -0700

    edit filter functionality to be more concise

--- client/src/components/ProductPages/Products.tsx
+++ client/src/components/ProductPages/Products.tsx
@@ -3,31 +3,37 @@ import { products } from '../../data/data';
 import { IProducts } from '../../types';
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import productFilter from '../../helpers/productFilter';
+import { productFilter, productSorter } from '../../helpers/productFilter';
 
 const Products = () => {
   const { category } = useParams<{ category: string }>();
   const [isMobile, setIsMobile] = useState<boolean>(false);
-  const [productCategory, setProductCategory] = useState<string | undefined>(
-    category
-  );
-  const [productSize, setProductSize] = useState<string | undefined>(undefined);
+  const [filters, setFilters] = useState<{ category?: string; size?: string }>({
+    category: category,
+    size: 'All sizes',
+  });
+
+  const [sort, setSort] = useState<string | undefined>('none');
   const [filteredProducts, setFilteredProducts] = useState<IProducts[]>([]);
 
   useEffect(() => {
-    setFilteredProducts(
-      productFilter(products, { productCategory, productSize })
-    );
-  }, [productCategory, productSize]);
+    setFilteredProducts(productFilter(products, filters));
+  }, [filters]);
+
+  useEffect(() => {
+    const filtered = [...filteredProducts];
+    setFilteredProducts(productSorter(filtered, sort));
+  }, [sort]);
 
   const handleResize = () => {
     window.innerWidth >= 850 ? setIsMobile(false) : setIsMobile(true);
   };
 
-  const onChangeHandler = (e: React.ChangeEvent, type: string) => {
-    if (type === 'category')
-      setProductCategory((e.target as HTMLInputElement).value);
-    if (type === 'size') setProductSize((e.target as HTMLInputElement).value);
+  const onChangeHandler = (e: React.ChangeEvent) => {
+    const type = (e.target as HTMLInputElement).name;
+    const value = (e.target as HTMLInputElement).value;
+    setFilters({ ...filters, [type]: value });
+    if (type === 'sortBy') setSort(value);
   };
 
   useEffect(() => {
@@ -37,47 +43,63 @@ const Products = () => {
   return (
     <div>
       <p className="flex justify-center pt-8 pb-5 text-heading lg:text-5xl md:text-5xl sm:text-4xl xs:text-4xl">
-        {productCategory}
+        {filters.category}
       </p>
-      <div className="flex flex-row">
-        <div className="flex flex-col">
-          <label htmlFor="categories" className="p-2 ml-10">
-            Pick a category:
-          </label>
-          <select
-            className="w-full max-w-xs ml-10 select"
-            name="categories"
-            value={productCategory}
-            onChange={e => onChangeHandler(e, 'category')}
-          >
-            <option disabled selected>
-              Category
-            </option>
-            <option value={'All products'}>All products</option>
-            <option value={'Single stickers'}>Single stickers</option>
-            <option value={'Sticker packs'}>Sticker packs</option>
-          </select>
-          <label htmlFor="categories" className="p-2 ml-10">
-            Pick a size:
-          </label>
-          <select
-            className="w-full max-w-xs ml-10 select"
-            name="sizes"
-            value={productSize}
-            onChange={e => onChangeHandler(e, 'size')}
-          >
-            <option disabled selected>
-              Size
-            </option>
-            <option value={'All sizes'}>All sizes</option>
-            <option value={'S'}>S</option>
-            <option value={'M'}>M</option>
-            <option value={'L'}>L</option>
-          </select>
+      <div className="flex justify-center w-full">
+        <div className="flex flex-row justify-around w-3/4">
+          <div className="flex flex-row">
+            <div className="flex flex-col mx-5">
+              <label htmlFor="categories" className="p-2 ml-10">
+                Pick a category:
+              </label>
+              <select
+                className="w-full max-w-xs ml-10 select"
+                name="category"
+                onChange={onChangeHandler}
+              >
+                <option disabled>Category</option>
+                <option value={'All products'}>All products</option>
+                <option value={'Single stickers'}>Single stickers</option>
+                <option value={'Sticker packs'}>Sticker packs</option>
+              </select>
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="categories" className="p-2 ml-10">
+                Pick a size:
+              </label>
+              <select
+                className="w-full max-w-xs ml-10 select"
+                name="size"
+                onChange={onChangeHandler}
+              >
+                <option disabled>Size</option>
+                <option value={'All sizes'}>All sizes</option>
+                <option value={'S'}>S</option>
+                <option value={'M'}>M</option>
+                <option value={'L'}>L</option>
+              </select>
+            </div>
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="categories" className="p-2 ml-10">
+              Sort by
+            </label>
+            <select
+              className="w-full max-w-xs ml-10 select"
+              name="sortBy"
+              onChange={onChangeHandler}
+            >
+              <option disabled>Sort</option>
+              <option value={'none'}>None</option>
+              <option value={'priceasc'}>Price asc</option>
+              <option value={'pricedesc'}>Price desc</option>
+              <option value={'newest'}>Newest</option>
+              <option value={'oldest'}>Oldest</option>
+            </select>
+          </div>
         </div>
       </div>
-
-      <div className="flex flex-wrap justify-between p-5">
+      <div className="flex flex-wrap justify-start p-5">
         {!filteredProducts.length ? (
           <div>No products available</div>
         ) : (
--- client/src/helpers/productFilter.ts
+++ client/src/helpers/productFilter.ts
@@ -1,25 +1,36 @@
 import { IProducts } from '../types';
-interface IFilter {
-  productCategory: string | undefined;
-  productSize: string | undefined;
-}
+
 const productFilter = (
   products: IProducts[],
-  { productCategory, productSize }: IFilter
+  filter: { category?: string; size?: string }
 ) => {
   //could turn this into a big ternary
-  if (productCategory === 'All products' && productSize === 'All sizes')
+  if (filter.category === 'All products' && filter.size === 'All sizes')
     return products;
 
-  if (productCategory === 'All products')
-    return products.filter(product => product.size === productSize);
+  if (filter.category === 'All products')
+    return products.filter(product => product.size === filter.size);
 
-  if (productSize === 'All sizes')
-    return products.filter(product => product.category === productCategory);
+  if (filter.size === 'All sizes')
+    return products.filter(product => product.category === filter.category);
 
   return products.filter(
-    item => item.category === productCategory && item.size === productSize
+    item => item.category === filter.category && item.size === filter.size
   );
 };
 
-export default productFilter;
+const productSorter = (products: IProducts[], sortBy: string | undefined) => {
+  if (sortBy === 'priceasc') return products.sort((a, b) => a.price - b.price);
+  if (sortBy === 'pricedesc') return products.sort((a, b) => b.price - a.price);
+  if (sortBy === 'newest')
+    return products.sort(
+      (a, b) => a.createdAt.getTime() - b.createdAt.getTime()
+    );
+  if (sortBy === 'oldest')
+    return products.sort(
+      (a, b) => b.createdAt.getTime() - a.createdAt.getTime()
+    );
+  return products;
+};
+
+export { productFilter, productSorter };
