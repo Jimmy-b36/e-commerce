e80f129637ece7a0bad39081f66782f787994162
jimmy-btv <jimmy.btv22@gmail.com>
2022-10-06 11:34:53 -0700

    add custom stickers route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,33 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+} from 'react-router-dom';
 
 import Home from './pages/Home';
 import ProductList from './pages/ProductList';
 import Product from './pages/Product';
 import Registration from './pages/Registration';
 import Checkout from './pages/Checkout';
+import Custom from './pages/Custom';
 import Pay from './components/Pay';
 import Success from './pages/Success';
 
 function App() {
+  const user = false;
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products/:category" element={<ProductList />} />
         <Route path="/product/:productId" element={<Product />} />
+        <Route path="/custom" element={<Custom />} />
+        <Route
+          path="/register"
+          element={user ? <Navigate to="/" replace /> : <Registration />}
+        />
+
         <Route
           path="/pay"
           element={
