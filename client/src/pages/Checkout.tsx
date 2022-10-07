9d26f2a475c1612bc04f9205917d545263d3f17b
jimmy-btv <jimmy.btv22@gmail.com>
2022-10-07 09:08:31 -0700

    add redux to update cart

--- client/src/pages/Checkout.tsx
+++ client/src/pages/Checkout.tsx
@@ -1,11 +1,14 @@
 import NavBar from '../components/NavBar';
 import Announcement from '../components/Announcement';
-import Newsletter from '../components/Newsletter';
 import Footer from '../components/Footer';
-import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { IReduxCartProduct } from '../types';
+import { Link } from 'react-router-dom';
+import Pay from '../components/Pay';
 
 const Checkout = () => {
-  const [cartTotal, setCartTotal] = useState<number>(10);
+  const cart = useSelector((state: any) => state.cart);
+
   let tax: number = 0;
   const shipping: string = '5.70';
   return (
@@ -16,7 +19,7 @@ const Checkout = () => {
         <p className="text-heading">Your bag</p>
         <div className="flex items-center justify-center w-1/2 mt-5 2xl:justify-around xs:w-full xs:flex-col lg:flex-col lg:w-1/2">
           <a href="" className="m-2 text-xl underline ">
-            Shopping bag (2)
+            Shopping bag ({cart.quantity})
           </a>
           <a href="" className="m-2 text-xl underline">
             Your wishlist (0)
@@ -25,93 +28,51 @@ const Checkout = () => {
 
         <div className="flex flex-col items-center justify-between w-3/4 mt-5 2xl:flex-row lg:w-full">
           <div className="flex flex-col">
-            <div className="flex flex-col items-center w-full p-4 border-b border-black 2xl:flex-row xl:flex-row lg:flex-row">
-              <div className="w-full 2xl:w-1/4 lg:w-1/2">
-                <img
-                  src="https://github.com/jimmy-btv/e-commerce/blob/main/client/src/assets/images/Round_Sticker_Mockup_003.jpg?raw=true"
-                  alt="Sticker mockup"
-                />
-              </div>
-              <div className="flex flex-col items-center justify-center w-full border-b border-black 2xl:w-1/2 2xl:border-r 2xl:border-b-0 xl:border-b-0 lg:border-b-0 2xl:border-black">
-                <p className="p-2">
-                  <strong className="text-xl">Product:</strong> Mockup round
-                  sticker
-                </p>
-                <p className="p-2">
-                  <strong className="text-xl">Product ID:</strong> 4235893427895
-                </p>
-                <p className="p-2">
-                  <strong className="text-xl">Material:</strong> Weather proof
-                </p>
-                <p className="p-2">
-                  <strong className="text-xl">Size:</strong> 12cm x 12cm
-                </p>
-              </div>
-              <div className="flex flex-col items-center justify-center px-10 2xl:border-r 2xl:border-black">
-                <div className="flex flex-row py-2">
-                  <strong className="text-xl">Quantity:</strong>{' '}
-                  <button>
-                    <i className="px-2 fa-solid fa-minus"></i>
-                  </button>
-                  <p className="flex items-center px-5 mx-2 border border-black rounded-lg">
-                    2
-                  </p>
-                  <button>
-                    <i className="px-2 fa-solid fa-plus"></i>
-                  </button>
+            {cart.products.map((product: IReduxCartProduct) => (
+              <div className="flex flex-col items-center w-full p-4 border-b border-black 2xl:flex-row xl:flex-row lg:flex-row">
+                <div className="w-full 2xl:w-1/4 lg:w-1/2">
+                  <img src={product.img} alt={product.alt} />
                 </div>
-                <p className="py-2">
-                  <strong className="text-xl">Individual price:</strong> $10
-                </p>
-                <p className="py-2">
-                  <strong className="text-xl">Total price:</strong> $20
-                </p>
-              </div>
-            </div>
-
-            <div className="flex flex-col items-center w-full p-4 border-b border-black 2xl:flex-row xl:flex-row lg:flex-row">
-              <div className="w-full 2xl:w-1/4 lg:w-1/2">
-                <img
-                  src="https://github.com/jimmy-btv/e-commerce/blob/main/client/src/assets/images/Round_Sticker_Mockup_003.jpg?raw=true"
-                  alt="Sticker mockup"
-                />
-              </div>
-              <div className="flex flex-col items-center justify-center w-full border-b border-black 2xl:w-1/2 2xl:border-r 2xl:border-b-0 xl:border-b-0 lg:border-b-0 2xl:border-black">
-                <p className="p-2">
-                  <strong className="text-xl">Product:</strong> Mockup round
-                  sticker
-                </p>
-                <p className="p-2">
-                  <strong className="text-xl">Product ID:</strong> 4235893427895
-                </p>
-                <p className="p-2">
-                  <strong className="text-xl">Material:</strong> Weather proof
-                </p>
-                <p className="p-2">
-                  <strong className="text-xl">Size:</strong> 12cm x 12cm
-                </p>
-              </div>
-              <div className="flex flex-col items-center justify-center px-10 2xl:border-r 2xl:border-black">
-                <div className="flex flex-row py-2">
-                  <strong className="text-xl">Quantity:</strong>{' '}
-                  <button>
-                    <i className="px-2 fa-solid fa-minus"></i>
-                  </button>
-                  <p className="flex items-center px-5 mx-2 border border-black rounded-lg">
-                    2
+                <div className="flex flex-col items-center justify-center w-full border-b border-black 2xl:w-1/2 2xl:border-r 2xl:border-b-0 xl:border-b-0 lg:border-b-0 2xl:border-black">
+                  <p className="p-2">
+                    <strong className="text-xl">Product:</strong>{' '}
+                    {product.title}
                   </p>
-                  <button>
-                    <i className="px-2 fa-solid fa-plus"></i>
-                  </button>
+                  <p className="p-2">
+                    <strong className="text-xl">Product ID:</strong>{' '}
+                    {product._id}
+                  </p>
+                  <p className="p-2">
+                    <strong className="text-xl">Material:</strong> Weather proof
+                  </p>
+                  <p className="p-2">
+                    <strong className="text-xl">Size:</strong> {product.size}
+                  </p>
                 </div>
-                <p className="py-2">
-                  <strong className="text-xl">Individual price:</strong> $10
-                </p>
-                <p className="py-2">
-                  <strong className="text-xl">Total price:</strong> $20
-                </p>
+                <div className="flex flex-col items-center justify-center px-10 2xl:border-r 2xl:border-black">
+                  <div className="flex flex-row py-2">
+                    <strong className="text-xl">Quantity:</strong>{' '}
+                    <button>
+                      <i className="px-2 fa-solid fa-minus"></i>
+                    </button>
+                    <p className="flex items-center px-5 mx-2 border border-black rounded-lg">
+                      {product.quantity}
+                    </p>
+                    <button>
+                      <i className="px-2 fa-solid fa-plus"></i>
+                    </button>
+                  </div>
+                  <p className="py-2">
+                    <strong className="text-xl">Individual price:</strong> $
+                    {product.price}
+                  </p>
+                  <p className="py-2">
+                    <strong className="text-xl">Total price:</strong> $
+                    {product.price * product.quantity}
+                  </p>
+                </div>
               </div>
-            </div>
+            ))}
           </div>
 
           <div className="flex flex-col items-center justify-center w-1/2 text-center xs:w-full">
@@ -120,27 +81,30 @@ const Checkout = () => {
             </p>
             <p className="my-2">
               {' '}
-              <strong className="text-xl">SubTotal:</strong> ${cartTotal}
+              <strong className="text-xl">SubTotal:</strong> ${cart.subtotal}
             </p>
             <p className="my-2">
               <strong className="text-xl">Shipping:</strong> ${shipping}
             </p>
             <p className="my-2">
               <strong className="text-xl">Tax:</strong> $
-              {(tax = cartTotal * 0.12)}
+              {(tax = cart.subtotal * 0.12).toFixed(2)}
             </p>
             <p className="my-2">
               <strong className="text-xl">Total:</strong> $
-              {cartTotal + tax + +shipping}
+              {Number(cart.subtotal + tax + +shipping).toFixed(2)}
             </p>
           </div>
         </div>
         <div className="flex justify-around w-1/2 mt-5 xs:w-full xs:flex-col lg:flex-col lg:w-1/2">
-          <button className="mb-5 text-white btn 2xl:mb-0 xl:mb-0">
-            Continue shopping
-          </button>
+          <Link to={'/'}>
+            <button className="mb-5 text-white btn 2xl:mb-0 xl:mb-0">
+              Continue shopping
+            </button>
+          </Link>
+
           <button className="mt-5 text-white bg-green-500 border-transparent btn 2xl:mt-0 xl:mt-0 hover:bg-green-700 hover:border-transparent">
-            Checkout now
+            <Pay cart={cart}>CHECKOUT NOW</Pay>
           </button>
         </div>
       </div>
